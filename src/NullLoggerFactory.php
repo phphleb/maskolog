483551b0b8e0409adc065d245e249bb5363d82d1
Foma <fomiash@yandex.ru>
2026-02-18 15:39:25 +0300

    chore: update null handler construct

--- src/NullLoggerFactory.php
+++ src/NullLoggerFactory.php
@@ -9,9 +9,15 @@ namespace Maskolog;
 
 use Monolog\Handler\NullHandler;
 use Monolog\Logger;
+use Psr\Log\LogLevel;
 
 class NullLoggerFactory extends AbstractManagedLoggerFactory
 {
+    public function __construct()
+    {
+        parent::__construct(LogLevel::INFO, true);
+    }
+
     protected function createMonologLogger(): Logger
     {
         $logger = new Logger('null');
