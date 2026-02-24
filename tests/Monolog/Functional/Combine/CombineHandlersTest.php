a7eb2f46dccc96102f1b1a1fa0817262a9fe770f
Foma <fomiash@yandex.ru>
2026-02-24 14:42:32 +0300

    chore: add combine handlers tests

--- tests/Monolog/Functional/Combine/CombineHandlersTest.php
+++ tests/Monolog/Functional/Combine/CombineHandlersTest.php
@@ -5,7 +5,6 @@ declare(strict_types=1);
 namespace MaskologLoggerTests\Monolog\Functional\Combine;
 
 use Maskolog\Enums\PasswordMaskingStatus;
-use Maskolog\ExampleLogger;
 use Maskolog\Logger;
 use MaskologLoggerTests\Monolog\Functional\LoggerFactory\CombineManagedLoggerFactory;
 use MaskologLoggerTests\Monolog\Functional\TestHandlerConverterTrait;
@@ -44,6 +43,46 @@ class CombineHandlersTest extends TestCase
         $this->assertCount(1, $handlers);
 
         $this->assertCount(1, $unmaskHandlers);
+
+        $mask = PasswordMaskingStatus::MASKED_PASSWORD;
+        $resultMaskMessage = "Text using password: {$mask}";
+        $resultUnmaskMessage = "Text using password: secret_password";
+
+        $unmaskResult = $this->convertHandler($testHandler);
+        $this->assertCount(1, $unmaskResult);
+        $log = current($unmaskResult);
+
+        $this->assertEquals($resultMaskMessage, $log->message);
+
+        $maskResult = $this->convertHandler($unmaskTestHandler);
+        $this->assertCount(1, $maskResult);
+        $log = current($maskResult);
+
+        $this->assertEquals($resultUnmaskMessage, $log->message);
+    }
+
+    /**
+     * @throws JsonException
+     */
+    public function testLoggerHandlers2(): void
+    {
+        $unmaskTestHandler = new TestHandler();
+        $unmaskTestHandler->setFormatter(new JsonFormatter(JsonFormatter::BATCH_MODE_JSON, true, false, true));
+        $testHandler = new TestHandler();
+        $testHandler->setFormatter(new JsonFormatter(JsonFormatter::BATCH_MODE_JSON, true, false, true));
+
+        $fabric = new CombineManagedLoggerFactory($testHandler, $unmaskTestHandler);
+        $logger = new Logger($fabric);
+
+        $logger->info('Text using password: {password}', ['password' => 'secret_password']);
+
+        $unmaskHandlers = $logger->getUnmaskingLogger()->getHandlers();
+
+        $handlers = $logger->getMaskingLogger()->getHandlers();
+
+        $this->assertCount(1, $unmaskHandlers);
+
+        $this->assertCount(1, $handlers);
 
         $mask = PasswordMaskingStatus::MASKED_PASSWORD;
         $resultMaskMessage = "Text using password: {$mask}";
